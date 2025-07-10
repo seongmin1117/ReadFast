8c897cb4147a6d1fe89c7c476bddea3d23b532c8
seongmin <seongmin1117@gmail.com>
2025-07-10 16:36:16 +0900

    Initial commit

---
+++ src/main/java/com/baro13/readfast/ReadFastApplication.java
@@ -0,0 +1,13 @@
+package com.baro13.readfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadFastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baro13/readfast/ReadFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baro13.readfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadFastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
